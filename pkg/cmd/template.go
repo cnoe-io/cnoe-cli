f0d9bac29a8377f06dd3864e71fbec70f283fe12
Nima Kaviani <nkaviani@amazon.com>
2023-06-30 16:41:26 -0700

    add the template command

---
+++ pkg/cmd/template.go
@@ -0,0 +1,326 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/cnoe-io/cnoe-cli/pkg/models"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	defDir  = "aws-resources"
+	KindXRD = "CompositeResourceDefinition"
+	KindCRD = "CustomResourceDefinition"
+)
+
+var (
+	inputDir     string
+	outputDir    string
+	templatePath string
+	namespaced   bool
+)
+
+func init() {
+	rootCmd.AddCommand(templateCmd)
+	templateCmd.Flags().StringVarP(&inputDir, "inputDir", "i", "", "input directory for CRDs and XRDs to be templatized")
+	templateCmd.Flags().StringVarP(&outputDir, "outputDir", "o", "", "output directory for backstage templates to be stored in")
+	templateCmd.Flags().StringVarP(&templatePath, "templatePath", "t", "scaffolding/template.yaml", "path to the template to be augmented with backstage info")
+	templateCmd.Flags().BoolVarP(&namespaced, "namespaced", "n", false, "whether or not resources are namespaced")
+
+	templateCmd.MarkFlagRequired("inputDir")
+	templateCmd.MarkFlagRequired("outputDir")
+	templateCmd.MarkFlagRequired("templatePath")
+}
+
+var (
+	templateCmd = &cobra.Command{
+		Use:   "template",
+		Short: "Generate backstage templates from CRD/XRD",
+		Long:  `Generate backstage templates from supplied CRD and XRD definitions`,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if !isDirectory(inputDir) || !isDirectory(outputDir) {
+				return errors.New("inputDir and ouputDir entries need to be directories")
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			defs := defs(inputDir, 0)
+
+			output, err := writeSchema(
+				outputDir,
+				defs,
+			)
+			if err != nil {
+				return err
+			}
+
+			err = writeToTemplate(
+				templatePath,
+				outputDir,
+				output.Resources, 0)
+
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+)
+
+type cmdOutput struct {
+	Templates []string
+	Resources []string
+}
+
+func defs(dir string, depth int) []string {
+	if depth > 2 {
+		return nil
+	}
+
+	var out []string
+	base, _ := filepath.Abs(dir)
+	files, _ := ioutil.ReadDir(base)
+	for _, file := range files {
+		f := filepath.Join(base, file.Name())
+		stat, _ := os.Stat(f)
+		if stat.IsDir() {
+			out = append(out, defs(f, depth+1)...)
+			continue
+		}
+
+		out = append(out, f)
+	}
+
+	return out
+}
+
+func writeSchema(outputPath string, defs []string) (cmdOutput, error) {
+	out := cmdOutput{
+		Templates: make([]string, 0),
+		Resources: make([]string, 0),
+	}
+
+	templateOutputDir := fmt.Sprintf("%s/%s", outputPath, defDir)
+	_, err := os.Stat(templateOutputDir)
+	if os.IsNotExist(err) {
+		// Directory doesn't exist, so create it
+		err := os.MkdirAll(templateOutputDir, 0755)
+		if err != nil {
+			return cmdOutput{}, err
+		}
+		fmt.Println("Directory created successfully!")
+	} else if err != nil {
+		return cmdOutput{}, err
+	}
+
+	for _, def := range defs {
+		data, err := ioutil.ReadFile(def)
+		if err != nil {
+			continue
+		}
+
+		var doc models.Definition
+		err = yaml.Unmarshal(data, &doc)
+		if err != nil {
+			continue
+		}
+
+		if !isXRD(doc) && !isCRD(doc) {
+			continue
+		}
+
+		fmt.Printf("foud: %s\n", def)
+		var resourceName string
+		if doc.Spec.ClaimNames != nil {
+			resourceName = doc.Spec.ClaimNames.Kind
+		} else {
+			resourceName = fmt.Sprintf("%s.%s", doc.Spec.Group, doc.Spec.Names.Kind)
+		}
+
+		var value map[string]interface{}
+
+		v := doc.Spec.Versions[0].Schema.OpenAPIV3Schema.Properties["spec"]
+		if v == nil {
+			value = doc.Spec.Versions[0].Schema.OpenAPIV3Schema.Properties
+		} else {
+			value, err = ConvertMap(v)
+			if err != nil {
+				fmt.Printf("failed %s: %s \n", def, err.Error())
+				continue
+			}
+		}
+
+		obj := &unstructured.Unstructured{
+			Object: make(map[string]interface{}, 0),
+		}
+		unstructured.SetNestedSlice(obj.Object, ConvertSlice([]string{resourceName}), "properties", "awsResources", "enum")
+		unstructured.SetNestedMap(obj.Object, value, "properties", "config")
+		unstructured.SetNestedField(obj.Object, fmt.Sprintf("%s configuration options", resourceName), "properties", "config", "title")
+
+		// setting GVK for the resource
+		if len(doc.Spec.Versions) > 0 {
+			unstructured.SetNestedMap(obj.Object, map[string]interface{}{
+				"type":        "string",
+				"description": "APIVersion for the resource",
+				"default":     fmt.Sprintf("%s/%s", doc.Spec.Group, doc.Spec.Versions[0].Name),
+			},
+				"properties", "apiVersion")
+			unstructured.SetNestedMap(obj.Object, map[string]interface{}{
+				"type":        "string",
+				"description": "Kind for the resource",
+				"default":     doc.Spec.Names.Kind,
+			},
+				"properties", "kind")
+		}
+
+		// add a property to define the namespace for the resource
+		if namespaced {
+			unstructured.SetNestedMap(obj.Object, map[string]interface{}{
+				"type":        "string",
+				"description": "Namespace for the resource",
+				"namespace":   "default",
+			},
+				"properties", "namespace")
+		}
+
+		wrapperData, err := yaml.Marshal(obj.Object)
+		if err != nil {
+			fmt.Printf("failed %s: %s \n", def, err.Error())
+			continue
+		}
+
+		template := fmt.Sprintf("%s/%s.yaml", templateOutputDir, strings.ToLower(resourceName))
+		err = ioutil.WriteFile(template, []byte(wrapperData), 0644)
+		if err != nil {
+			fmt.Printf("failed %s: %s \n", def, err.Error())
+			continue
+		}
+
+		out.Templates = append(out.Templates, template)
+		out.Resources = append(out.Resources, resourceName)
+	}
+
+	return out, nil
+}
+
+func writeToTemplate(templateFile string, outputPath string, resources []string, position int) error {
+	templateData, err := ioutil.ReadFile(templateFile)
+	if err != nil {
+		return err
+	}
+
+	var doc models.Template
+	err = yaml.Unmarshal(templateData, &doc)
+	if err != nil {
+		return err
+	}
+
+	dependencies := struct {
+		AwsResources struct {
+			OneOf []map[string]interface{} `yaml:"oneOf,omitempty"`
+		} `yaml:"awsResources,omitempty"`
+	}{}
+
+	awsResources := struct {
+		Type string   `yaml:"type"`
+		Enum []string `yaml:"enum"`
+	}{
+		Type: "string",
+		Enum: resources,
+	}
+
+	for _, resource := range resources {
+		dependencies.AwsResources.OneOf = append(dependencies.AwsResources.OneOf, map[string]interface{}{
+			"$yaml": fmt.Sprintf("aws-resources/%s.yaml", strings.ToLower(resource)),
+		})
+	}
+
+	doc.Spec.Parameters[position].Properties["awsResources"] = awsResources
+	doc.Spec.Parameters[position].Dependencies = dependencies
+
+	outputData, err := yaml.Marshal(&doc)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/test.yaml", outputPath), outputData, 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Template successfully written.")
+	return nil
+}
+
+func ConvertSlice(strSlice []string) []interface{} {
+	var ifaceSlice []interface{}
+	for _, s := range strSlice {
+		ifaceSlice = append(ifaceSlice, s)
+	}
+	return ifaceSlice
+}
+
+func ConvertMap(originalData interface{}) (map[string]interface{}, error) {
+	originalMap, ok := originalData.(map[interface{}]interface{})
+	if !ok {
+		return nil, errors.New("failed to convert to interface map")
+	}
+
+	convertedMap := make(map[string]interface{})
+
+	for key, value := range originalMap {
+		strKey, ok := key.(string)
+		if !ok {
+			// Skip the key if it cannot be converted to string
+			continue
+		}
+
+		switch v := value.(type) {
+		case map[interface{}]interface{}:
+			// If the value is a nested map, recursively convert it
+			var err error
+			convertedMap[strKey], err = ConvertMap(v)
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("failed to convert for key %s", strKey))
+			}
+		case int:
+			convertedMap[strKey] = int64(v)
+		case int32:
+			convertedMap[strKey] = int64(v)
+		default:
+			// Otherwise, add the key-value pair to the converted map
+			convertedMap[strKey] = v
+		}
+	}
+
+	return convertedMap, nil
+}
+
+func isXRD(m models.Definition) bool {
+	return m.Kind == KindXRD
+}
+
+func isCRD(m models.Definition) bool {
+	return m.Kind == KindCRD
+}
+
+func isDirectory(path string) bool {
+	// Get file information
+	info, err := os.Stat(path)
+	if err != nil {
+		// Error occurred, path does not exist or cannot be accessed
+		return false
+	}
+
+	// Check if the path is a directory
+	return info.Mode().IsDir()
+}
---
+++ pkg/models/structs.go
@@ -0,0 +1,81 @@
+package models
+
+import "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+type Metadata struct {
+	Name   string            `json:"name"`
+	Labels map[string]string `json:"labels,omitempty"`
+}
+
+type Spec struct {
+	ClaimNames *struct {
+		Kind string `json:"kind"`
+	} `json:"claimNames,omitempty"`
+	Group string `json:"group"`
+	Names struct {
+		Kind string `json:"kind"`
+	} `json:"names"`
+	Versions []struct {
+		Name   string `json:"name"`
+		Schema struct {
+			OpenAPIV3Schema struct {
+				Properties map[string]interface{} `json:"properties"`
+			} `yaml:"openAPIV3Schema"`
+		} `json:"schema"`
+	} `json:"versions"`
+}
+
+type Definition struct {
+	Kind       string   `json:"kind"`
+	APIVersion string   `json:"apiVersion"`
+	Metadata   Metadata `json:"metadata"`
+	Spec       Spec     `json:"spec"`
+}
+
+type Resources struct {
+	Enum []string `json:"enum"`
+}
+
+type Config struct {
+	Type       string                     `json:"type"`
+	Title      string                     `json:"title"`
+	Properties *unstructured.Unstructured `json:"properties,omitempty"`
+}
+
+type Props struct {
+	AWSResources Resources `json:"awsResources"`
+	Config       Config    `json:"config"`
+}
+
+type Wrapper struct {
+	Properties Props `json:"properties"`
+}
+
+type Template struct {
+	ApiVersion string `yaml:"apiVersion"`
+	Kind       string `yaml:"kind"`
+	Metadata   struct {
+		Name        string `yaml:"name"`
+		Title       string `yaml:"title"`
+		Description string `yaml:"description"`
+	} `yaml:"metadata"`
+	Spec struct {
+		Owner      string `yaml:"owner"`
+		Type       string `yaml:"type"`
+		Parameters []struct {
+			Properties   map[string]interface{} `yaml:"properties"`
+			Dependencies struct {
+				AwsResources struct {
+					OneOf []map[string]interface{} `yaml:"oneOf,omitempty"`
+				} `yaml:"awsResources,omitempty"`
+			} `yaml:"dependencies,omitempty"`
+		} `yaml:"parameters"`
+
+		Steps []struct {
+			Id     string                 `yaml:"id"`
+			Name   string                 `yaml:"name"`
+			Action string                 `yaml:"action"`
+			Input  map[string]interface{} `yaml:"input"`
+		} `yaml:"steps"`
+	} `yaml:"spec"`
+}
